1cdf2b0f9c1dd5029dc6291cd5f2e7416d0a56c5
akageun <akageun@gmail.com>
2018-10-25 14:25:15 +0900

    spring-boot-todo-app-1 First Commit

---
+++ src/main/java/kr/geun/t/app/TodoApplication.java
@@ -0,0 +1,12 @@
+package kr.geun.t.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+}
