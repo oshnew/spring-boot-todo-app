6653db30cc9fac311c709fc27fa5cf0c92b93922
akageun <akageun@gmail.com>
2018-10-26 17:30:23 +0900

    [spring-boot-todo-app-4] H2 Database 세팅  - 기본 Table Entity 생성

---
+++ src/main/java/kr/geun/t/app/todo/entity/TodoEntity.java
@@ -0,0 +1,56 @@
+package kr.geun.t.app.todo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 할일 관련 메인 테이블
+ *
+ * @author akageun
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "todo")
+public class TodoEntity {
+
+	/**
+	 * 할일 PK
+	 */
+	@Id
+	@Column(name = "todo_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long todoId;
+
+	/**
+	 * 할일 내용
+	 */
+	@Column(name = "content", length = 1024, nullable = false)
+	private String content;
+
+	/**
+	 * 할일의 상태값
+	 *  - EX) NOT_YET, COMPLETE 등...
+	 */
+	@Column(name = "status_cd", nullable = false)
+	private String statusCd;
+
+	/**
+	 * 생성일시
+	 */
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	/**
+	 * 수정일시
+	 */
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
+}
---
+++ src/main/java/kr/geun/t/app/todo/entity/TodoRefEntity.java
@@ -0,0 +1,55 @@
+package kr.geun.t.app.todo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 할일 관련 참조 Entity
+ *
+ * @author akageun
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "todo_reference")
+public class TodoRefEntity {
+
+	/**
+	 * 참조 할일 PK
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ref_id")
+	private Long refId;
+
+	/**
+	 * 상위 할일 번호
+	 */
+	@Column(name = "parent_todo_id")
+	private Long parentTodoId;
+
+	/**
+	 * 참조한 할일 번호
+	 */
+	@Column(name = "ref_todo_id")
+	private Long refTodoId;
+
+	/**
+	 * 생성일시
+	 */
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	/**
+	 * 수정일시
+	 */
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
+}
