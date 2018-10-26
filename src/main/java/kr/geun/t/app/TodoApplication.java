de4a8b9ba467adbb9d8212d551f4716d6a11f4fa
akageun <akageun@gmail.com>
2018-10-26 16:44:47 +0900

    [spring-boot-todo-app-2] 초기 환경 세팅

--- src/main/java/kr/geun/t/app/TodoApplication.java
+++ src/main/java/kr/geun/t/app/TodoApplication.java
@@ -3,9 +3,19 @@ package kr.geun.t.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 프로젝트 Entry Class
+ *
+ * @author akageun
+ */
 @SpringBootApplication
 public class TodoApplication {
 
+	/**
+	 * Main Method
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
